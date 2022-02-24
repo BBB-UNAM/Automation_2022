454375289681502c5b41450ca9ae7b92b3bbc2f3
Alberto Picasso <picassoa12@gmail.com>
2022-02-23 21:58:29 -0600

    Add new web Element Clases

---
+++ src/test/java/com/WebElementsIdentifers/DemoWebShopBookElements.java
@@ -0,0 +1,8 @@
+package com.WebElementsIdentifers;
+
+public class DemoWebShopBookElements {
+    public static String idSortBy = "products-orderby";
+    public static String idDisplay = "products-pagesize";
+    public static String idViewAs = "products-viewmode";
+
+}
---
+++ src/test/java/com/WebElementsIdentifers/DemoWebShopProductBar.java
@@ -0,0 +1,12 @@
+package com.WebElementsIdentifers;
+
+public class DemoWebShopProductBar {
+
+    public static String xpathBooks = "//ul[@class='top-menu']//a[@href='/books']";
+    public static String xpathComputers = "//ul[@class='top-menu']//a[@href='/computers']";
+    public static String xpathElectronics = "//ul[@class='top-menu']//a[@href='/electronics']";
+    public static String xpathApparealShoes = "//ul[@class='top-menu']//a[@href='/apparel-shoes']";
+    public static String xpathDigitalDounlead = "//ul[@class='top-menu']//a[@href='/digital-downloads']";
+    public static String xpathJewelry = "//ul[@class='top-menu']//a[@href='/jewelry']";
+    public static String xpathGiftCards = "//ul[@class='top-menu']//a[@href='/gift-cards']";
+}
